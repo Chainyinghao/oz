555d891b2be92cbbbcb4d8336aeda52bd90c8e0d
Chainyinghao <dyinghao@outlook.com>
2020-04-16 20:55:04 +0800

    oz

--- oz/oz-setup.sh
+++ oz/oz-setup.sh
@@ -1,10 +1,12 @@
 #/bin/bash
 
-TAG=`rpm -q docker-ce`
+rpm -q docker-ce
+TAG=`echo $?`
+
 DIR=`pwd`
 DOCKER_IMAGE=${DIR}/images/docker_images/oz_image.tar
 STATUS=$(systemctl status docker | grep Active | awk '{print $2}')
-if [ $TAG = 1 ];then
+if [[ $TAG -eq 1 ]];then
     yum install -y docker-ce
     systemctl enable docker
     systemctl restart docker
@@ -12,7 +14,8 @@ fi
 if [[ ${STATUS} != "active" ]];then
     systemctl start docker
 fi
-#docker load --input $DOCKER_IMAGE
+
+docker load --input $DOCKER_IMAGE
 
 VERSION=`docker images | grep oz_centos|awk '{print $2}'`
 docker run -d --name oz_centos -it --privileged=true \
